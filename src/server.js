3034c7d27c91cbf6ac79a4904ce9ed61e2907398
NI-PA <91149653+NI-PA@users.noreply.github.com>
2024-02-08 07:51:16 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dbConnection from "./config/databaseConnector.js";
 import loggerMiddleware from "./middleware/loggerMiddleware.js";
 
-const app = express();
+const app = express( );
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,8 @@ app.use(routes);
 app.use((req, res, next) => {
   res.status(404);
   next(new Error("Page Not found"));
-});
+}
+       );
 
 app.use((error, req, res, next) => {
   res.send({ message: error.message, data: null, success: false });
